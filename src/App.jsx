75652f887915d1a9ba911885187e8309cdb82e6c
NILESH <nileshnimawat121@gmail.com>
2025-02-23 18:20:10 +0530

    updated error once again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Section from "./components/Section";
 import "./App.css";
