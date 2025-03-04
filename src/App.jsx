b9a062b4445788327b76830a591c24b16659bcd1
NILESH <nileshnimawat121@gmail.com>
2025-03-04 14:46:50 +0530

    Revert " add playlists and bugs"
    This reverts commit f20c85a35985083eec9d29ff7d4494b656ae98cd.

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,8 @@ function App() {
   return (
     <div className="flex flex-col h-screen scroll-smooth">
       <Navbar isOpen={isOpen} setIsOpen={setIsOpen}/>
-      <div className="flex flex-grow bg-black ">
-        <Sidebar location={location} likedSongs={likedSongs} isOpen={isOpen}  />
+      <div className="flex flex-grow bg-black">
+        <Sidebar location={location} likedSongs={likedSongs} isOpen={isOpen} setIsOpen={setIsOpen} />
         <Routes>
           <Route
             path="/"
