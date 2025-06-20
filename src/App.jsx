97d19e8fe8757e19500c60b9fbf09f560d10430a
NILESH <nileshnimawat121@gmail.com>
2025-06-20 12:31:22 +0530

    finally all errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useDispatch, useLocation, MusicPlayer, useSelector, useGetAllMusics
   ,useGetLoggedInUser, useGetAllPlaylists,
-  Toaster
+  Toaster,
+  useNavigate
  } from "./utils/lib";
 
 import AppRoutes from "./routes/AppRoutes";
@@ -14,6 +15,7 @@ function App() {
   useGetLoggedInUser();
   useGetAllPlaylists();
   const location = useLocation();
+  const navigate = useNavigate();
 const loading = useSelector((state) => state.loading.isLoading);
   const loggedInUser = useSelector((state)=>state.user.user);
 
