1df4303e849c34d2f6b08f6529c8e3af41670a65
Nileshnimawat <142086042+Nileshnimawat@users.noreply.github.com>
2025-06-20 14:40:00 +0530

    Update useGetAllMusics.js

--- src/hooks/useGetAllMusics.js
+++ src/hooks/useGetAllMusics.js
@@ -1,3 +1,9 @@
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';Add commentMore actions
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllMusics } from '../store/musicSlice';
+import { GET_MUSICS } from '@/utils/constants';
 export const useGetAllMusics = () => {
   const dispatch = useDispatch();
 
